08b80b0fede57dd0a301d275cd5faae6f081d6a6
ThanhAnIT <anthanh264@gmail.com>
2019-06-20 16:32:42 +0700

    Update script.js

--- chat/script.js
+++ chat/script.js
@@ -8,6 +8,18 @@ function randomEmoji() {
   var randomIndex = Math.floor(Math.random() * possibleEmojis.length);
   return possibleEmojis[randomIndex];
 }
+
+
+function GetURLParameter(sParam) {
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) {
+            return sParameterName[1];
+        }
+    }
+}​
 var name = GetURLParameter('id');
 
 const emoji = randomEmoji();
